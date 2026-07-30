17b747fd3d7097b61e01d49d6bd3e0229ba3b87a
frknecn3 <furkanduhannuman@gmail.com>
2026-07-30 23:39:23 +0300

    feat(poly): enhance PolyConnectionComponent with book tracking and logging

--- frontend/src/app/(HomePage)/PolyConnectionComponent.tsx
+++ frontend/src/app/(HomePage)/PolyConnectionComponent.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+'use client';
+
+import { usePolymarketBook } from '@/hooks/connectToPoly'
+import React, { useEffect } from 'react'
 
 type Props = {}
 
 const PolyConnectionComponent = (props: Props) => {
-  return (
-    <div>PolyConnectionComponent</div>
-  )
+
+    const { books, connected } = usePolymarketBook();
+
+    useEffect(()=>{
+        console.log("books changed")
+    },[books])
+
+    console.log(books)
+
+    return (
+        <div className='flex-1 border-r-4 border-r-sky-400'>
+            <span className='w-full'>
+
+            </span>
+        </div>
+    )
 }
 
 export default PolyConnectionComponent
--- frontend/src/app/(HomePage)/page.tsx
+++ frontend/src/app/(HomePage)/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import OkxConnectionComponent from './OkxConnectionComponent'
+import PolyConnectionComponent from './PolyConnectionComponent'
+import ArbWidget from './ArbWidget'
 
 type Props = {}
 
@@ -7,8 +9,10 @@ const HomePage = (props: Props) => {
   return (
     <div>
       <h1 className='text-4xl font-bold text-center h-40 flex items-center justify-center'>Connect To OKX</h1>
-      <div className='flex justify-center items-center'>
-        <OkxConnectionComponent />
+      <div className='flex justify-center items-center gap-8 w-120 m-auto min-h-80'>
+        {/* <OkxConnectionComponent />
+        <PolyConnectionComponent /> */}
+        <ArbWidget />
       </div>
     </div>
   )
